f165a329fd07b8567bf86786f7b1ff2fdbd3880c
edatrix <lespath@gmail.com>
2014-11-03 21:25:14 -0700

    adds tiny form to create new apple

--- app/controllers/apples_controller.rb
+++ app/controllers/apples_controller.rb
@@ -4,6 +4,12 @@ class ApplesController < ApplicationController
     @apples = Apple.all
   end
 
+  def create
+    @apple = Apple.new(apple_params)
+    @apple.save
+    redirect_to apples_path
+  end
+
   private
 
   def apple_params
